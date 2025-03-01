0b239e0cbb589b49280f90b8e2d281562d0d86d7
David Mayorga <David.Mayorga@adidas.com>
2025-02-28 21:40:19 -0500

    Hello angular signals

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { CounterPageComponent } from './pages/counter/components/counter-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+
+  {
+    path: '',
+    component: CounterPageComponent
+  }
+
+
+];
---
+++ src/app/pages/counter/components/counter-page.component.ts
@@ -0,0 +1,25 @@
+import { Component, signal } from "@angular/core";
+
+@Component({
+  templateUrl: './counter-page.component.html'
+})
+export class CounterPageComponent {
+
+
+  counter = 15;
+
+  counterSignal = signal(10);
+
+  increaseBy(value: number) {
+    // this.counter += value;
+
+    this.counterSignal.update( current => current + value );
+  }
+
+  resetCounter() {
+    // this.counter = 0;
+
+    this.counterSignal.set(0)
+  }
+
+}
