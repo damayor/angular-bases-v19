ed345750a86bd475ee08b0f8cc37dfcd05b9393a
Mayorga, David <David.Mayorga@adidas.com>
2025-03-19 15:11:43 -0500

    using microcomponetes and service

--- src/app/pages/dragonball-super/dragonball-super-page.component.ts
+++ src/app/pages/dragonball-super/dragonball-super-page.component.ts
@@ -1,9 +1,7 @@
-// import { DBZService } from './../services/dbz.service';
-// import { Character } from './../interfaces/character.interface';
-import { NgClass } from '@angular/common';
-import { Component, computed, signal } from '@angular/core';
+import { Component,inject} from '@angular/core';
 import { CharacterListComponent } from '../../components/dragon-ball/character-list/character-list.component';
 import { AddCharacterComponent } from "../../components/dragon-ball/add-character/add-character.component";
+import { DragonBallService } from '../../services/dragonball.service';
 
 
 interface Character {
@@ -14,7 +12,7 @@ interface Character {
 
 @Component({
   selector: 'dbz-super-main-page',
-  imports: [NgClass, CharacterListComponent, AddCharacterComponent],
+  imports: [CharacterListComponent, AddCharacterComponent],
   templateUrl: './dragonball-super-page.component.html',
 
 })
@@ -22,41 +20,6 @@ interface Character {
 
 export class DragonBallSuperPageComponent {
 
-  // constructor( private dbzService: DBZService) {  }
-
-  name = signal('')
-  power = signal(0)
-
-  characters = signal<Character[]>([
-    {
-      id: 123,
-      name:'Krillin',
-      power: 2000
-    },
-    {
-      id: 145,
-      name: 'Goku',
-      power: 9000
-    }
-  ])
-
-
-  powerClasses = computed(() => {
-    return {
-      'text-danger': true,
-    }
-  })
-
-
-
-
-  addCharacter(character: Character) {
-    this.characters.update( (list) => [...list, character]);
-    console.log('llamado desde el padre')
-
-  }
-
-
-
+  public dragonballService = inject(DragonBallService);
 
 }
